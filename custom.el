11775e04aabaefaa12c2f877578142b0d71a9d66
damian-t-p <damian.pavlyshyn@gmail.com>
2019-05-15 08:47:37 -0700

    Initial commit
    Included what I believe will install a full customised version of my
    emacs configuration. This includes a whitelist .gitignore

---
+++ custom.el
@@ -0,0 +1,93 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-electric-left-right-brace nil)
+ '(LaTeX-section-hook
+   (quote
+    (LaTeX-section-heading LaTeX-section-title LaTeX-section-section)))
+ '(LaTeX-verbatim-environments (quote ("verbatim" "verbatim*" "lstlisting")))
+ '(TeX-command-list
+   (quote
+    (("TeX" "%(PDF)%(tex) %(file-line-error) %(extraopts) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+      (plain-texo-mode texinfo-mode ams-tex-mode)
+      :help "Run plain TeX")
+     ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil
+      (latex-mode doctex-mode)
+      :help "Run LaTeX")
+     ("Makeinfo" "makeinfo %(extraopts) %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with Info output")
+     ("Makeinfo HTML" "makeinfo %(extraopts) --html %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with HTML output")
+     ("AmSTeX" "amstex %(PDFout) %(extraopts) %`%S%(mode)%' %t" TeX-run-TeX nil
+      (ams-tex-mode)
+      :help "Run AMSTeX")
+     ("ConTeXt" "%(cntxcom) --once --texutil %(extraopts) %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt once")
+     ("ConTeXt Full" "%(cntxcom) %(extraopts) %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt until completion")
+     ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
+     ("Biber" "biber %s" TeX-run-Biber nil t :help "Run Biber")
+     ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
+     ("Print" "%p" TeX-run-command t t :help "Print the file")
+     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
+     ("File" "%(o?)dvips %d -o %f " TeX-run-dvips t t :help "Generate PostScript file")
+     ("Dvips" "%(o?)dvips %d -o %f " TeX-run-dvips nil t :help "Convert DVI file to PostScript")
+     ("Dvipdfmx" "dvipdfmx %d" TeX-run-dvipdfmx nil t :help "Convert DVI file to PDF with dvipdfmx")
+     ("Ps2pdf" "ps2pdf %f" TeX-run-ps2pdf nil t :help "Convert PostScript file to PDF")
+     ("Glossaries" "makeglossaries %s" TeX-run-command nil t :help "Run makeglossaries to create glossary file")
+     ("Index" "makeindex %s" TeX-run-index nil t :help "Run makeindex to create index file")
+     ("upMendex" "upmendex %s" TeX-run-index t t :help "Run upmendex to create index file")
+     ("Xindy" "texindy %s" TeX-run-command nil t :help "Run xindy to create index file")
+     ("Check" "lacheck %s" TeX-run-compile nil
+      (latex-mode)
+      :help "Check LaTeX file for correctness")
+     ("ChkTeX" "chktex -v6 %s" TeX-run-compile nil
+      (latex-mode)
+      :help "Check LaTeX file for common mistakes")
+     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
+     ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
+     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
+     ("Other" "" TeX-run-command t t :help "Run an arbitrary command"))))
+ '(TeX-electric-math (quote ("\\(" . "\\)")))
+ '(TeX-insert-braces nil)
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(bmkp-last-as-first-bookmark-file "c:\\Users\\damia\\AppData\\Roaming\\.emacs.d\\bookmark")
+ '(cursor-type (quote box))
+ '(custom-enabled-themes (quote (darktooth)))
+ '(custom-safe-themes
+   (quote
+    ("88049c35e4a6cedd4437ff6b093230b687d8a1fb65408ef17bfcf9b7338734f6" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
+ '(flyspell-tex-command-regexp
+   "\\(\\(begin\\|end\\)[ 	]*{\\|\\(cite[a-z*]*\\|label\\|[cC]?ref\\|eqref\\|usepackage\\|documentclass\\)[ 	]*\\(\\[[^]]*\\]\\)?{[^{}]*\\)")
+ '(font-latex-math-environments
+   (quote
+    ("display" "displaymath" "equation" "eqnarray" "gather" "math" "multline" "align" "alignat" "xalignat" "xxalignat" "flalign" "gather")))
+ '(inhibit-startup-screen t)
+ '(ispell-dictionary "british")
+ '(ispell-program-name "D:\\Program Files\\Aspell\\bin\\aspell.exe")
+ '(line-move-visual t)
+ '(package-selected-packages
+   (quote
+    (projectile smart-mode-line latex-extra focus paredit buffer-flip buffer-move phi-search yasnippet darktooth-theme dracula-theme magit web-mode helm expand-region dashboard peep-dired ace-jump-mode julia-repl julia-mode poly-R poly-markdown polymode multiple-cursors font-utils unicode-fonts list-packages-ext use-package ess adaptive-wrap auctex)))
+ '(preview-gs-command "GSWIN64C.EXE")
+ '(preview-image-type (quote pnm))
+ '(preview-inner-environments
+   (quote
+    ("Bmatrix" "Vmatrix" "aligned" "array" "bmatrix" "cases" "gathered" "matrix" "pmatrix" "smallmatrix" "split" "subarray" "vmatrix" "piecewise")))
+ '(reftex-ref-style-default-list (quote ("Default" "Cleveref")))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
---
+++ init.el
@@ -0,0 +1,209 @@
+;; Location: C:\Users\damia\AppData\Roaming\.emacs.d
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; Blocks added by customize interface
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  (require 'use-package))
+
+(use-package ace-jump-mode
+  :bind ("C-c SPC" . ace-jump-mode))
+
+(use-package adaptive-wrap
+  :hook (visual-line-mode . adaptive-wrap-prefix-mode))
+
+(use-package bookmark+
+  :load-path "lisp/bookmark+-master/")
+
+(use-package buffer-move
+  :bind (("<C-S-up>" . buf-move-up)
+	 ("<C-S-down>" . buf-move-down)
+	 ("<C-S-left>" . buf-move-left)
+	 ("<C-S-right>" . buf-move-right)))
+
+(use-package buffer-stack
+  :load-path "lisp"
+  :bind (("<f9>" . buffer-stack-bury)
+	 ("C-<f9>" . buffer-stack-bury-and-kill)
+	 ("<f10>" . buffer-stack-up)
+	 ("<f11>" . buffer-stack-down)
+	 ("<f12>" . buffer-stack-track)
+	 ("C-<f12>". buffer-stack-untrack)))
+
+(use-package dired+
+  :load-path "lisp"
+  :custom
+  (dired-dwim-target t))
+
+(use-package flyspell
+  :hook ((text-mode . flyspell-mode)
+	 (prog-mode . flyspell-prog-mode)))
+
+(use-package helm
+  :bind (("M-x" . helm-M-x)
+	 ("M-y" . helm-show-kill-ring)
+	 ("C-x b" . helm-mini)
+	 ("C-x C-f" . helm-find-files))
+  :custom
+  (helm-split-window-in-side-p t)
+  :config
+  (helm-mode 1))
+
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+	 ("C->" . mc/mark-next-like-this)
+	 ("C-<" . mc/mark-previous-like-this)
+	 ("C-=" . mc/mark-all-like-this-dwim)
+	 ("C-#" . mc/insert-numbers)
+	 ("M-<down-mouse-1>" . mc/add-cursor-on-click))
+  :custom
+  (mc/insert-numbers-default 1)
+  :init
+  (global-unset-key (kbd "M-<down-mouse-1>")))
+
+(use-package org
+  :mode (("\\.org$" . org-mode))
+  :hook ((org-mode . visual-line-mode))
+  :custom
+  (org-hide-emphasis-markers t))
+
+(use-package paredit
+  :hook ((emacs-lisp-mode . paredit-mode)
+	 (lisp-mode . paredit-mode)
+	 (list-interaction-mode . paredit-mode)))
+
+(use-package phi-search
+  :bind (("C-s" . phi-search)
+	 ("C-r" . phi-search-backward)))
+
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+
+(use-package reftex
+  :after tex
+  :hook (LaTeX-mode . turn-on-reftex)
+  :custom
+  (reftex-plug-into-AUCTeX t)
+  :config
+  (eval-after-load
+      "latex"
+    '(TeX-add-style-hook
+      "cleveref"
+      (lambda ()
+	(if (boundp 'reftex-ref-style-alist)
+	    (add-to-list
+	     'reftex-ref-style-alist
+	     '("Cleveref" "cleveref"
+	       (("\\cref" ?c) ("\\Cref" ?C) ("\\cpageref" ?d) ("\\Cpageref" ?D)))))
+	(reftex-ref-style-activate "Cleveref")
+	(TeX-add-symbols
+	 '("cref" TeX-arg-ref)
+	 '("Cref" TeX-arg-ref)
+	 '("cpageref" TeX-arg-ref)
+	 '("Cpageref" TeX-arg-ref)))))
+
+  (add-to-list 'reftex-label-alist
+	       '("equation" ?e "eq:" "~\\ref{%s}" t
+		 (regexp "equations?" "eqs?\\." "eqn\\." "Gleichung\\(en\\)?"  "Gl\\.")))
+
+  (setq reftex-default-bibliography '("D:/Users/Damian/Local tex files/bibtex/bib/mendeley/Covariance-Proportionality.bib")))
+
+(use-package smart-mode-line
+  :config
+  (sml/setup)
+  (add-to-list 'sml/replacer-regexp-list
+	       '("^.*Documents/Uni/PhD/" ":PhD:" t)))
+
+(use-package tex
+  :ensure auctex
+  :custom
+  (TeX-parse-self t) ; Enable parse on load.
+  (TeX-auto-save t) ; Enable parse on save.
+  (font-latex-match-reference-keywords
+      '(("cref" "{")
+	("Cref" "{")
+	("citet" "{"))) ;; Recognise as a \ref-type commans
+  :hook ((LaTeX-mode . visual-line-mode)
+	 (LaTeX-mode . linum-mode)
+	 (LaTeX-mode . LaTeX-math-mode)))
+
+(use-package web-mode
+  :mode ("\\.html?\\'" "\\.css\\'"))
+
+(use-package yasnippet
+  :init
+  (setq closing-brackets
+	'(("(" . ")")
+	  ("[" . "]")
+	  ("\\\{" . "\\\}")))
+  (defun close-bracket (opener)
+    (cond ((assoc opener closing-brackets)
+	   (cdr (assoc opener closing-brackets)))
+	  ((string-prefix-p "\\l" opener)
+	   (concat "\\r" (substring opener 2 nil)))
+	  (t ".")))
+  :config
+  (yas-reload-all)
+  :hook ((LaTeX-mode . yas-minor-mode)
+	 (julia-mode . yas-minor-mode)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom splitting functions ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ 
+(defun vsplit-last-buffer ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil)
+  (switch-to-next-buffer)
+  )
+(defun hsplit-last-buffer ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (switch-to-next-buffer)
+  )
+ 
+(global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+
+;; Move lines around
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "M-N") 'move-line-down)
+(global-set-key (kbd "M-P") 'move-line-up)
